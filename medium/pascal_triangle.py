7810731202080a3d782bdf77260e9ae6395c3e78
Lisa <lisa.li72159@gmail.com>
2019-12-04 12:57:49 -0500

    Pascal's Triangle

---
+++ medium/pascal_triangle.py
@@ -0,0 +1,15 @@
+Pascal's Triangle
+
+Given an input, find array of the nth line of Pascal's triangle.
+
+def generate_pascal(num):
+    if num == 0: return [1]
+    if num == 1: return [1,1]
+
+    prevRow = generate_pascal(num - 1)
+    result = []
+
+    for i in range(len(prevRow - 1))
+        result.append(prevRow[i] + prevRow[i + 1])
+    
+    return [1] + result + [1]
