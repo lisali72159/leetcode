59d5fc8d74b5bdc7022952dbb18892163cd274c4
Lisa <lisa.li72159@gmail.com>
2019-12-08 16:21:00 -0500

    Steps same position

--- medium/1269_steps.py
+++ medium/1269_steps.py
@@ -1,4 +1,10 @@
-def numWays(steps, arrLen, pos= 0):
+def numWays(steps, arrLen, pos= 0, memo=None):
+    if memo is None: memo = {}
+    key = (steps, pos) # use tuple as keys for dictionary
+    if key in memo: return memo[key]
+
+    # empty default object for memo
+
     # negative base case
     # if i is out of bounds, return 0
     if pos < 0 or pos >= arrLen or steps < 0:
@@ -7,11 +13,15 @@ def numWays(steps, arrLen, pos= 0):
         return 1
     
     totalWays = 0
-    totalWays += numWays(steps - 1, arrLen, pos - 1) # go left
-    totalWays += numWays(steps - 1, arrLen, pos) # stay
-    totalWays += numWays(steps - 1, arrLen, pos + 1) # go right
+    totalWays += numWays(steps - 1, arrLen, pos - 1, memo) # go left
+    totalWays += numWays(steps - 1, arrLen, pos, memo) # stay
+    totalWays += numWays(steps - 1, arrLen, pos + 1, memo) # go right
+    memo[key] = totalWays
     return totalWays
 
 numWays(3,2) # 4
 numWays(0,0) # 1
+
+
+
 
