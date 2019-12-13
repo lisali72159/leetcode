1c661124813f41806ab57775ebe87a51811e2a2f
Lisa <lisa.li72159@gmail.com>
2019-12-13 15:40:42 -0500

    Contains duplicates

---
+++ medium/217_contains_duplicates.py
@@ -0,0 +1,15 @@
+# Contains duplicates: Return true if there are duplicates in given array, else return false
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+        dict = {}
+        
+        for i in range(len(nums)):
+            if nums[i] not in dict:
+                dict[nums[i]] = 1
+            else:
+                dict[nums[i]] += 1
+                
+                if dict[nums[i]] == 2:
+                    return True
+            
+        return False
