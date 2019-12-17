ebe60b60ab2b95cba94ce28b1838ff08713b395e
Lisa <lisa.li72159@gmail.com>
2019-12-17 14:08:33 -0500

    Word Break

--- medium/139_word_break.js
+++ medium/139_word_break.js
@@ -18,11 +18,11 @@ function wordBreak(string, wordDict) {
     let table = new Array(string.length + 1).fill(false);
     table[0] = true;
 
-    for (let i = 0; i < string.length; i++) {
+    for (let i = 0; i < table.length; i++) {
         if (table[i] === false) {
             continue;
         }
-        for (let j = i + 1; j < string.length; j++) {
+        for (let j = i + 1; j < table.length; j++) {
             let word = string.slice(i,j);
             if (wordDict.includes(word)){
                 table[j] = true;
