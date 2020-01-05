173cd5a2971216715221d899ae6df44d607d31ea
Lisa <lisa.li72159@gmail.com>
2020-01-05 16:28:46 -0500

    Add two nums

--- medium/2_add_two_nums.py
+++ medium/2_add_two_nums.py
@@ -4,17 +4,18 @@ class ListNode:
         self.val = x
         self.next = None
 
-two = ListNode(2)
-four = ListNode(4)
-three = ListNode(3)
-two.next = four
-four.next = three
+a1 = ListNode(2)
+a2 = ListNode(4)
+a3 = ListNode(3)
+a1.next = a2
+a2.next = a3
 # (2 -> 4-> 3)
 
-five = ListNode(5)
-six = ListNode(6)
-four_ = ListNode(4)
-five.next = six.next = four_
+b1 = ListNode(5)
+b2 = ListNode(6)
+b3 = ListNode(4)
+b1.next = b2
+b2.next = b3
 # (5 -> 6 -> 4)
 
 
@@ -26,9 +27,9 @@ def addTwoNumbers(l1, l2):
     result = None
 
     while curr1 or curr2:
-        val1 = curr1.val if cur1 else 0 #Python ternary
-        val2 = curr2.val if cur2 else 0 
-        sum = val1.val + val2.val + carry
+        val1 = curr1.val if curr1 else 0 #Python ternary
+        val2 = curr2.val if curr2 else 0 
+        sum = val1 + val2 + carry
         carry = sum // 10
         digit = sum % 10
         nextNode = ListNode(digit)
@@ -53,3 +54,16 @@ def addTwoNumbers(l1, l2):
 # Edge-case scenarios: different lengths of 2 linked lists, result is different length if there is a carry 
 # Time Complexity: O(n) where n is the length of longest list or O(max(m,n)) 
 # Space Complexity: O(n) where n is the length of longest list
+
+def _addTwoNumbers(l1, l2, carry = 0):
+    if l1 is None and l2 is None:
+        return None
+
+    val1 = l1.val if l1 else 0
+    val2 = l2.val if l2 else 0
+    sum = val1 + val2 + carry
+    carry = sum // 10
+    digit = sum % 10
+    newNode = ListNode(digit)
+
+    newNode.next = addTwoNumbers(l1.next, l2.next, carry)
