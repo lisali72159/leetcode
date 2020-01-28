1064e864ec4d64a311dfbb795d4085e95729900d
Lisa <lisa.li72159@gmail.com>
2020-01-28 16:18:25 -0500

    More leetcode problems

---
+++ easy/climbing_stairs.py
@@ -0,0 +1,27 @@
+# You are climbing a stair case. It takes n steps to reach to the top.
+
+# Each time you can either climb 1 or 2 or 3 steps. In how many distinct ways can you climb to the top?
+
+# Note: Given n will be a positive integer.
+
+# Example 1:
+
+# Input: 3
+# Output: 3
+
+
+
+def climbingSteps(n):
+    if n == 1: return 1
+    if n == 2: return 2
+
+    twoAway = 1
+    oneAway = 2
+
+    for i in range(3, n + 1):
+        current = twoAway + oneAway
+        twoAway = oneAway
+        oneAway = prev 
+    
+    return current
+
---
+++ easy/valid_palindromes_II.py
@@ -0,0 +1,12 @@
+ def validPalindrome(self, s: str) -> bool:
+        start_idx = 0
+        end_idx = len(s) - 1
+        
+        while start_idx <= end_idx:
+            if s[start_idx] != s[end_idx]:
+                s1 = s[start_idx: end_idx]
+                s2 = s[start_idx + 1: end_idx + 1]
+                return s1 == s1[:: -1] or s2 == s2[:: -1]
+            start_idx += 1
+            end_idx -=1
+        return True
